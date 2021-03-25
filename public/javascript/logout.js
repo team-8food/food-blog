f0ff84ffafcdf15e3c04df0d8b12c49385f24b71
DSnoddy <douglassnoddyjr@gmail.com>
2021-03-24 20:49:21 -0800

    feature config and public

---
+++ public/javascript/logout.js
@@ -0,0 +1,29 @@
+async function logout() {
+    const response = await fetch('/api/users/logout', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('#logout').addEventListener('click', logout);
+  
+  async function mobilelogout() {
+    const response = await fetch('/api/users/logout', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('#mobile-logout').addEventListener('click', mobilelogout);
