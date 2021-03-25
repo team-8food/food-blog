45218574727ec81e53205a31ed259db29d4f09f9
Samuel <samuel@Samuels-MacBook-Pro.local>
2021-03-24 21:23:51 -0700

    transfer of starter files

---
+++ server.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("../foodblog/foodBlog/config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: { maxAge: 600000 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+const helpers = require('../foodblog/foodBlog/utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+ 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('../foodblog/foodBlog/controllers'));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+}); 
