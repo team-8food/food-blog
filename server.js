9c4a9c89df2c4e11f9b806c7ae915698d2cdc7ae
Ben Tan <btan87@gmail.com>
2021-03-25 18:54:35 -0700

    updated server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("../foodblog/foodBlog/config/connection");
+const sequelize = require("../food-blog/config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -21,7 +21,7 @@ const sess = {
 
 app.use(session(sess));
 
-const helpers = require('../foodblog/foodBlog/utils/helpers');
+const helpers = require('../food-blog/utils/helpers');
 
 const hbs = exphbs.create({ helpers });
  
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('../foodblog/foodBlog/controllers'));
+app.use(require('../food-blog/controllers'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
