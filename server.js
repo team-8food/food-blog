32299fa5b989d00d5faf29aa469e88b44d9f6afc
Ben Tan <btan87@gmail.com>
2021-03-28 17:48:49 -0700

    updated server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("../food-blog/config/connection");
+const sequelize = require("/food-blog/config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
